824bc3534a03adb79bc765f701fa0ae6d0328150
lvargascol <lvargascol@gmail.com>
2022-09-07 20:37:01 -0300

    [ADD]Progressive Web App with next-pwa

--- next.config.js
+++ next.config.js
@@ -1,24 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['placeimg.com', 'api.lorem.space'],
   },
-  // env: {
-  //   customKey: 'customValue',    
-  // },
-  // basePath: '/dist',
-  // compress: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/hola',
-  //       destination: 'https://lvargascol.com',
-  //       permanent: true,
-  //     }
-  //   ]
-  // }
-}
+};
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
